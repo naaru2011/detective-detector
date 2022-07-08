417c7780a433b25c16f183d1b792c1a93dd4fa3a
naaru2011 <83050539+naaru2011@users.noreply.github.com>
2022-07-07 20:00:23 -0700

    Add files via upload

---
+++ compsition.js
@@ -0,0 +1,36 @@
+function preload(){
+    loadImage("compisition-book.jpg");
+}
+
+function setup() {
+    createCanvas(600, 500, 0, 0);
+    document.getElementById("nil").innerHTML = "Status: Detecting Objects";
+    objectDetector = ml5.objectDetector('crossed', modelLoaded);
+
+}
+function modelLoaded() {
+    console.log("Model Loaded!");
+    status = true;
+    objectDetector.detect(img, gotResult);
+
+    
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    }
+    console.log(results);
+}
+
+function draw(){
+if (status != "") {
+    for (i=0; i < objects.length; i++){
+        document.getElementById("status").innerHTML = "There are 2 big objects and Cocossd has only identified one"
+        text(objects[i].label + " " + "%", objects[i].x, objects[i].y);
+    }
+        
+}
+}
+
+
